b766ac690423736d9f8ce765743b563a73a61e1d
talkingpotatover2 <104803907+talkingpotatover2@users.noreply.github.com>
2022-07-18 18:07:15 +0900

    Add files via upload

---
+++ baekjoon/Deque.java
@@ -0,0 +1,71 @@
+package com.baekjoon;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Deque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
+		Scanner sc = new Scanner(System.in);
+		int head = 0;
+		
+		//출력횟수 입력받기
+		int num = sc.nextInt();
+		while(num > 0) {
+			String str = sc.next();
+			switch(str) {
+				case "push_front": 
+					int X = sc.nextInt();
+					deque.addFirst(X);
+					break;
+				case "push_back":
+					X = sc.nextInt();
+					deque.addLast(X);
+					break;
+				case "pop_front":
+					if(deque.size() == 0) {
+						System.out.println(-1);
+					}else {
+						System.out.println(deque.peekFirst());
+						deque.removeFirst();
+					}
+					break;
+				case "pop_back":
+					if(deque.size() == 0) {
+						System.out.println(-1);
+					}else {
+						System.out.println(deque.peekLast());
+						deque.removeLast();
+					}
+					break;
+				case "size":
+					System.out.println(deque.size());
+					break;
+				case "empty":
+					if(deque.size() == 0) {
+						System.out.println(1);
+					}else {
+						System.out.println(0);
+					}
+					break;
+				case "front":
+					if(deque.size() == 0) {
+						System.out.println(-1);
+					}else {
+						System.out.println(deque.peekFirst());
+					}
+					break;
+				case "back":
+					if(deque.size() == 0) {
+						System.out.println(-1);
+					}else {
+						System.out.println(deque.peekLast());
+					}
+			}
+			num--;
+		}
+	}
+
+}
