9d8297084c7aa796d6919d4b47fb44f41636457a
talkingpotatover2 <104803907+talkingpotatover2@users.noreply.github.com>
2022-07-23 17:14:05 +0900

    Add files via upload

---
+++ baekjoon/WordReverse2.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class WordReverse2 {
+	//https://1-7171771.tistory.com/27
+	static void print(Stack st) {
+		while(!st.empty()) {
+			System.out.print(st.pop());
+		}
+	}
+
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		boolean check = false;
+		
+		for(int i=0; i<str.length(); i++) {
+			char c = str.charAt(i);
+			if(c == '<') {
+				check = true;
+				print(st);
+				System.out.print(c);
+			}else if(c == '>') {
+				check = false;
+				System.out.print(c);
+			}else if (check){
+				System.out.print(c);
+			}else {
+				if(c == ' ') {
+					print(st);
+					System.out.print(c);
+				}else {
+					st.push(c);
+				}
+			}
+		}
+		print(st);
+	}
+
+}
